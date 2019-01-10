021838bd97535d1f8e74d4822e4ac2552a1119aa
summer1874 <summer.mail1874@foxmail.com>
2019-01-10 20:34:58 +0800

    api

--- components/header.vue
+++ components/header.vue
@@ -1,36 +1,78 @@
 <template>
   <div class="header">
-    <div class="herder-active">
-      <div>1</div>
+    <div class="nav-navigation">
+      <div class="navigation-left">
+        <i class="el-icon-search"/>
+      </div>
+      <div class="navigation-centre">
+        <img 
+          src="../assets/img/logo.png" 
+          alt="">
+      </div>
+      <div class="navigation-right">
+        <i class="el-icon-goods"/>
+        <i class="el-icon-sold-out"/>
+        <i class="el-icon-star-on"/>
+      </div>
     </div>
-    <div class="nav">
-      <div 
-        v-for="item in 7"
-        :key="item"
-        class="nav-title">DEALS</div>
+    <div class="nav-menu">
+      <ul class="menu-list">
+        <li 
+          v-for="item in menuLsit"
+          :key="item.menu_name" 
+          class="menu-item">{{ item.menu_name }}</li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HEADNOPTION'
+  name: 'HEADNOPTION',
+  data() {
+    return {
+      menuLsit: []
+    }
+  },
+  async mounted() {
+    const { data } = await this.$axios.get(
+      'https://shop.wangoon.com/api/NetworkApi/menu_list_api'
+    )
+    this.menuLsit = data.resData
+  }
 }
 </script>
 
 <style socped lang="scss">
-.nav {
+.nav-menu > .menu-list {
+  padding: 0 40px;
   display: flex;
+  justify-content: space-around;
   background: #150d0f;
-  justify-content: center;
-  align-content: center;
-  .nav-title {
+  li {
+    list-style: none;
     line-height: 60px;
     color: #bfa69a;
     font-weight: 700;
     font-size: 16px;
-    text-transform: uppercase;
+  }
+}
+.nav-navigation {
+  display: flex;
+  justify-content: space-between;
+  height: 90px;
+  padding: 0 30px;
+  background: #ffffff;
+  div {
     display: flex;
+    align-items: center;
+  }
+  img {
+    height: 100%;
+  }
+  i {
+    font-size: 30px;
+    margin: 0 10px;
   }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="index">
-    https://shop.wangoon.com/mall/home/homeColumn 返回数据
-    <div >{{ newColumn.data }}</div>
-    
+    <el-carousel 
+      :interval="4000" 
+      :height="bannerH +'px'"
+      arrow="always">
+      <el-carousel-item 
+        v-for="item in bannerList" 
+        :key="item.url">
+        <img 
+          :src="item.img" 
+          class="carousel-img">
+      </el-carousel-item>
+    </el-carousel>
   </div>
 </template>
 <style>
@@ -17,11 +26,42 @@
 
 <script>
 export default {
+  data() {
+    return {
+      bannerH: 200
+    }
+  },
   async asyncData({ $axios }) {
-    const newColumn = await $axios.$get(
-      'https://shop.wangoon.com/mall/home/homeColumn'
+    const params = {
+      params: {
+        code: 'H_1'
+      }
+    }
+    const { list } = await $axios.$get(
+      'https://shop.wangoon.com/mall/home/banner',
+      params
     )
-    return { newColumn }
+    return { bannerList: list }
+  },
+  mounted() {
+    this.setBannerH()
+    window.addEventListener(
+      'resize',
+      () => {
+        this.setBannerH()
+      },
+      false
+    )
+  },
+  methods: {
+    setBannerH() {
+      this.bannerH = document.body.clientWidth / 2.5
+    }
   }
 }
 </script>
+<style lang="scss" scoped>
+.carousel-img {
+  width: 100%;
+}
+</style>
