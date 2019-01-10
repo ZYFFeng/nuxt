7d3ed2d43328c37242f36b79d6cd012adbd591b8
summer1874 <summer.mail1874@foxmail.com>
2019-01-10 18:07:19 +0800

    first commit

---
+++ components/header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="header">
+    <div class="herder-active">
+      <div>1</div>
+    </div>
+    <div class="nav">
+      <div 
+        v-for="item in 7"
+        :key="item"
+        class="nav-title">DEALS</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HEADNOPTION'
+}
+</script>
+
+<style socped lang="scss">
+.nav {
+  display: flex;
+  background: #150d0f;
+  justify-content: center;
+  align-content: center;
+  .nav-title {
+    line-height: 60px;
+    color: #bfa69a;
+    font-weight: 700;
+    font-size: 16px;
+    text-transform: uppercase;
+    display: flex;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="index">
+    https://shop.wangoon.com/mall/home/homeColumn 返回数据
+    <div >{{ newColumn.data }}</div>
+    
+  </div>
+</template>
+<style>
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+</style>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const newColumn = await $axios.$get(
+      'https://shop.wangoon.com/mall/home/homeColumn'
+    )
+    return { newColumn }
+  }
+}
+</script>
